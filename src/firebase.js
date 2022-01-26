5696de4d3e97d7b6370c1cb14f802dae77521a19
jarekm04 <jarek_m04@wp.pl>
2022-01-26 17:36:38 +0100

    firebase fix

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,12 @@ import {getAuth} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC4SW0wDTBBw9JzaEAYQS6TwXtbI4vCP1Q",
-    authDomain: "projektdemo-1b4ad.firebaseapp.com",
-    databaseURL: "https://projektdemo-1b4ad-default-rtdb.firebaseio.com",
-    projectId: "projektdemo-1b4ad",
-    storageBucket: "projektdemo-1b4ad.appspot.com",
-    messagingSenderId: "1023060235555",
-    appId: "1:1023060235555:web:7c82888c31c61579962896"
+    apiKey: "AIzaSyBsTN60RRmvTwh4usrGpIbFSGcGnEEA9Nc",
+    authDomain: "twojtrenerpl-ae88d.firebaseapp.com",
+    projectId: "twojtrenerpl-ae88d",
+    storageBucket: "twojtrenerpl-ae88d.appspot.com",
+    messagingSenderId: "73061267259",
+    appId: "1:73061267259:web:f11b58c13e2a76f179691e"
 };
 
 // Initialize Firebase
