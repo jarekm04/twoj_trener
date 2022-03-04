a28bef620bb7923684d9cf368bef2cd9369718b8
jarekm04 <jarek_m04@wp.pl>
2022-03-04 19:36:36 +0100

    modal fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, {useEffect, useState} from 'react';
-import "./styles/base.scss";
+import React, {useState} from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import WelcomePage from "./components/LandingPage/WelcomePage";
 import HomePage from "./components/UserLogged/HomePage";
@@ -14,24 +13,22 @@ function App() {
     const [showSidebar, setShowSidebar] = useState(false);
     const [isUserLogged, setIsUserLogged] = useState(false);
 
-
     return (
-        <>
-            <Router>
-                { isUserLogged ?
-                    <HeaderUser showSidebar={showSidebar} setShowSidebar={setShowSidebar} setIsUserLogged={setIsUserLogged} />
-                    : <Header showSidebar={showSidebar} setShowSidebar={setShowSidebar} setIsUserLogged={setIsUserLogged} />
-                }
-                <Routes>
-                    <Route path="/" element={<WelcomePage />} />
-                    <Route path="/homepage" element={<HomePage />} />
-                    <Route path="/plan" element={<TrainingPlanUser />} />
-                    <Route path="/contact" element={<ContactUser />} />
-                    <Route path="/notes" element={<NotesUser />} />
-                    <Route path="/calculators" element={<CalculatorsUser />} />
-                </Routes>
-            </Router>
-        </>
+        <Router>
+            {isUserLogged ?
+                <HeaderUser showSidebar={showSidebar} setShowSidebar={setShowSidebar}
+                            setIsUserLogged={setIsUserLogged}/>
+                : <Header showSidebar={showSidebar} setShowSidebar={setShowSidebar} setIsUserLogged={setIsUserLogged}/>
+            }
+            <Routes>
+                <Route path="/" element={<WelcomePage/>}/>
+                <Route path="/homepage" element={<HomePage/>}/>
+                <Route path="/plan" element={<TrainingPlanUser/>}/>
+                <Route path="/contact" element={<ContactUser/>}/>
+                <Route path="/notes" element={<NotesUser/>}/>
+                <Route path="/calculators" element={<CalculatorsUser/>}/>
+            </Routes>
+        </Router>
     );
 }
 
